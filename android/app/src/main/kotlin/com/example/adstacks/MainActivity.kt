0f03a808998d48e172b3082d955cb89ec6d96932
Ajay008 <Ajaychauhan08122000@gmail.com>
2026-06-27 23:22:25 +0530

    deploy: base-href /adstacks/ for GitHub Pages

--- android/app/src/main/kotlin/com/example/adstacks/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.adstacks
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:adstacks/main.dart';
-
-void main() {
-  testWidgets('AdStacks dashboard smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const AdStacksApp());
-    expect(find.text('Top Rating Project'), findsOneWidget);
-  });
-}
